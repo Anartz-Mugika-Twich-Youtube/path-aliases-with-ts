2cd39b3ee1149843b0e7fae9b4f3be715ebb3057
Anartz Mugika Ledo <mugan86@gmail.com>
2022-12-12 19:50:09 +0100

    feat: tutorial template to start working

---
+++ src/server.ts
@@ -0,0 +1,75 @@
+import { createServer, Server as HTTPServer } from "http";
+import compression from "compression";
+import express, { Application } from "express";
+import { GraphQLSchema } from "graphql";
+import { ApolloServer } from "apollo-server-express";
+import depthLimit from 'graphql-depth-limit'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import expressPlayGround from "graphql-playground-middleware-express";
+
+class Server {
+  private app!: Application;
+  private httpServer!: HTTPServer;
+  private schema!: GraphQLSchema;
+  private readonly DEFAULT_PORT_SERVER =
+    process.env.PORT || 3003;
+  constructor(schema: GraphQLSchema) {
+    if (schema === undefined) {
+      throw new Error("Need GraphQL Schema to work in API GraphQL");
+    }
+    this.schema = schema;
+    this.initialize();
+  }
+
+  /**
+   * Inicializar todas las configuraciones establecidas en el servidor
+   */
+  private initialize() {
+    this.configExpress();
+    this.configApolloServer();
+    this.configRoutes();
+    this.createServer();
+  }
+
+  private configExpress() {
+    this.app = express();
+
+    this.app.use(compression());
+  }
+
+  private async configApolloServer() {
+    // COnfigurar el servidor apollo server
+
+    const server = new ApolloServer({
+      schema: this.schema,
+      introspection: true,
+      validationRules: [ depthLimit(4) ],
+      // Para que podamos tener disponible el playground en producción
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    });
+    await server.start();
+    server.applyMiddleware({ app: this.app, cors: true });
+  }
+
+  private configRoutes() {
+    this.app.get("/hello", (_, res) => {
+      res.send("Bienvenidos/as al curso de GraphQL desde 0");
+    });
+
+    this.app.get('/', expressPlayGround({ endpoint: '/graphql' }));
+  }
+
+  private createServer() {
+    this.httpServer = createServer(this.app);
+  }
+
+  listen(callback: (port: number) => void): void {
+    this.httpServer.listen(this.DEFAULT_PORT_SERVER, () => {
+      callback(+this.DEFAULT_PORT_SERVER);
+    });
+  }
+}
+
+export default Server;
+
+
